540485c2241533979083eb944ca6ff73391b0bb8
Anastasiya957 <anastasiyabarysheva95@gmail.com>
2017-09-29 23:50:03 +0300

    start to implement LinkedList class

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,64 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head;
+        this._tail;
+        this.head;
+        this.tail;
+    }
 
-    append(data) {}
+    append(data) {
+        var newNode = new Node(data);
+        if (this.length == 0) {  
+            this._head = newNode;
+            this._tail = newNode;
+        } else {
+            this._tail.next = newNode;
+            newNode.prev = this._tail;
+            this._tail = newNode;
+        }
+        this.length += 1;
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data; 
+    }
 
-    at(index) {}
+    at(index) {
+        var currentNode = this._head;
+        for (var i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        var newNode = new Node(data);
+        var currentNode = this._head;
+        for (var i = 1; i < index; i++) {
+            currentNode = currentNode.next;
+        }
 
-    isEmpty() {}
+        newNode.next = currentNode.next;
+        newNode.prev = currentNode;
+        currentNode.next = newNode;
+
+        this.length += 1;
+    }
+
+    isEmpty() {
+        if (this.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     clear() {}
 
